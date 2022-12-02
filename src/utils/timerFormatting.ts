2818147b52b6eb13b58fb2dd6cc948175685ccc0
jaeyeong815 <jae0ee@naver.com>
2022-12-02 21:34:02 +0900

    style: Timer 남은 분 시간 포매팅 수정

--- src/utils/timerFormatting.ts
+++ src/utils/timerFormatting.ts
@@ -1,6 +1,6 @@
 export const timerFormatting = (time: number) => {
   return {
-    min: String(time / 60).padStart(2, '0'),
+    min: String(Math.floor(time / 60)).padStart(2, '0'),
     sec: String(time % 60).padStart(2, '0'),
   };
 };

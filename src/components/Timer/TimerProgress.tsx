2f197f4472f8bddbc1b0a7e14dad4ea1d67f214c
jaeyeong815 <jae0ee@naver.com>
2022-12-02 21:34:53 +0900

    feat: Timer 시작, 정지, 리셋버튼 컴포넌트 분리 및 기능 구현

---
+++ src/components/Timer/TimerProgress.tsx
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import {
+  focusTimerState,
+  restTimerState,
+  timerTimeState,
+} from '../../recoil/timerState';
+import { useEffect, useState } from 'react';
+import { timeInfo } from '../../utils/timerTime';
+
+enum TimerState {
+  start = '진행중',
+  stop = '중단',
+  reset = '초기화',
+}
+
+function TimerProgress() {
+  const [timerIng, setTimerIng] = useState(false);
+  const timerTime = useRecoilValue(timerTimeState);
+  const setFocusTime = useSetRecoilState(focusTimerState);
+  const setRestTime = useSetRecoilState(restTimerState);
+
+  const timerStartHandle = () => {
+    setTimerIng(true);
+  };
+
+  const timerPauseHandle = () => {
+    setTimerIng(false);
+  };
+
+  const timerResetHandle = () => {
+    setTimerIng(false);
+    if (timerTime.twentyFive) {
+      setFocusTime(timeInfo.twentyFive.focusTime);
+      setRestTime(timeInfo.twentyFive.restTime);
+      return;
+    }
+    setFocusTime(timeInfo.fifty.focusTime);
+    setRestTime(timeInfo.twentyFive.restTime);
+  };
+
+  function Timer() {
+    useEffect(() => {
+      const timer = setInterval(() => {
+        setFocusTime((prev) => (prev -= 1));
+      }, 1000);
+      return () => {
+        clearInterval(timer);
+      };
+    }, []);
+    return (
+      <BtnWrapper>
+        <ProgressBtn name={TimerState.stop} onClick={timerPauseHandle}>
+          일시정지
+        </ProgressBtn>
+        <ProgressBtn name={TimerState.reset} onClick={timerResetHandle}>
+          처음으로
+        </ProgressBtn>
+      </BtnWrapper>
+    );
+  }
+
+  return (
+    <>
+      {timerIng ? (
+        <Timer />
+      ) : (
+        <ProgressBtn name={TimerState.start} onClick={timerStartHandle}>
+          시작하기
+        </ProgressBtn>
+      )}
+    </>
+  );
+}
+
+export default TimerProgress;
+
+const ProgressBtn = styled.button`
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+
+  font-size: 30px;
+  font-weight: 500;
+
+  color: white;
+  background-color: #3aa694;
+`;
+
+const BtnWrapper = styled.div`
+  display: flex;
+  column-gap: 20px;
+`;
---
+++ src/utils/timerTime.ts
@@ -0,0 +1,4 @@
+export const timeInfo = {
+  twentyFive: { focusTime: 1500, restTime: 300 },
+  fifty: { focusTime: 3000, restTime: 600 },
+};
